e583cc946a885fc6daf7c01854ba1f8356abc448
cuong <cuongtvk@vietqr.vn>
2024-01-23 12:05:25 +0700

    update

--- lib/commons/constants/configurations/numeral.dart
+++ lib/commons/constants/configurations/numeral.dart
@@ -2,7 +2,7 @@
 
 class Numeral {
   static const int DEFAULT_PIN_LENGTH = 6;
-  static const int DEFAULT_TIMEOUT_API = 60;
+  static const int DEFAULT_TIMEOUT_API = 600;
   static const double BANK_CARD_RATIO = (674 / 426);
 
   static const int ENV_TEST = 0;
