231cfe4c760ba1680ba18541deacd9b271e529d7
admin <duchieupham1709@gmail.com>
2024-06-12 17:09:07 +0700

    build web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9492c3aa080e3165911cded6c28f3362",
+const RESOURCES = {"flutter_bootstrap.js": "5b348b8653eac08dce47be281ad853a1",
 "version.json": "e4fe985f9fb3b074ab4690f22b388d4b",
 "splash/img/light-2x.png": "78b32172d739fcec40fd4ff9601192ec",
 "splash/img/dark-4x.png": "ce752977aef4637f4e5c40deb4a774fb",
@@ -15,10 +15,10 @@ const RESOURCES = {"flutter_bootstrap.js": "9492c3aa080e3165911cded6c28f3362",
 "splash/img/light-1x.png": "940040774642d53de6f71453e737a203",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "c94c38ff00a9d487c353a2d78989ea08",
-"index.html": "75611149bd1af17953414419e32884cf",
-"/": "75611149bd1af17953414419e32884cf",
+"index.html": "bbc7cb16a14acb5eefb18e3af5bf50bb",
+"/": "bbc7cb16a14acb5eefb18e3af5bf50bb",
 "firebase-messaging-sw.js": "ac580569feefa4089ae00ab086b89ada",
-"main.dart.js": "54362e6a3494f036cf6967753f4e6e88",
+"main.dart.js": "707543fcf6c26ae7b1590adb932b2a28",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "icons/favicon-16x16.png": "0852cf0bc1ac3fe43eb50cd8b6d0accd",
 "icons/favicon.ico": "f43709e9dd9a3265ef12c4f419e8de07",
