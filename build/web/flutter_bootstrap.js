ad903d0d3eb1856e459e3fe1a577825f9cc2e35d
admin <duchieupham1709@gmail.com>
2024-06-19 22:08:59 +0700

    build web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4246690692"
+    serviceWorkerVersion: "1235448386"
   }
 });
