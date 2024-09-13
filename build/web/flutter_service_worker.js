d17887e269440f9de8ec94b4a0c16e61f2b2080d
Smjle <kien.nh@vietqr.vn>
2024-09-13 08:30:02 +0700

    deploy web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "774369df218a488d34719f3700798c09",
+const RESOURCES = {"flutter_bootstrap.js": "99e148a4db204aca7a443c6ca8849cbe",
 "version.json": "e4fe985f9fb3b074ab4690f22b388d4b",
 "splash/img/light-2x.png": "78b32172d739fcec40fd4ff9601192ec",
 "splash/img/dark-4x.png": "ce752977aef4637f4e5c40deb4a774fb",
@@ -15,10 +15,10 @@ const RESOURCES = {"flutter_bootstrap.js": "774369df218a488d34719f3700798c09",
 "splash/img/light-1x.png": "940040774642d53de6f71453e737a203",
 "splash/splash.js": "123c400b58bea74c1305ca3ac966748d",
 "splash/style.css": "c94c38ff00a9d487c353a2d78989ea08",
-"index.html": "ade1deb6f5b61387c8a17eb6b3716fa5",
-"/": "ade1deb6f5b61387c8a17eb6b3716fa5",
+"index.html": "9f0c81061b97eedcc7d48ca84e94588e",
+"/": "9f0c81061b97eedcc7d48ca84e94588e",
 "firebase-messaging-sw.js": "ac580569feefa4089ae00ab086b89ada",
-"main.dart.js": "31df063fa7d360f63b7971b4d6792a57",
+"main.dart.js": "eba21b281a0622d63efca869de0fc8c8",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "icons/favicon-16x16.png": "0852cf0bc1ac3fe43eb50cd8b6d0accd",
 "icons/favicon.ico": "f43709e9dd9a3265ef12c4f419e8de07",
